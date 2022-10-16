0e8f9f5b1db61b9124d887062694964809e19619
Ujjwal <bhattaraiujjwal26@gmail.com>
2022-10-16 18:31:03 +0530

    major changes

---
+++ src/Components/Videos.jsx
@@ -0,0 +1,25 @@
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import { VideoCard, ChannelCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  console.log(videos);
+  if (!videos.length) return "loading...";
+  return (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
+      {videos?.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
